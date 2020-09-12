6b62d067ec3d61e01c0d73a0bc8854c7c75ec67e
Pazmy <fadhillah17azmy01@gmail.com>
2020-09-12 21:36:28 +0700

    Create Homescreen, add data and define router

---
+++ frontend/src/app.js
@@ -0,0 +1,22 @@
+import HomeScreen from "./screen/HomeScreen.js";
+import ProductScreen from "./screen/ProductScreen.js";
+import { parseRequestUrl } from "./utils.js";
+import Error404Screen from "./screen/Error4040Screen.js";
+
+const routes = {
+  "/": HomeScreen,
+  "/product/:id": ProductScreen,
+};
+const router = () => {
+  const request = parseRequestUrl();
+  const parseUrl =
+    (request.resource ? `/${request.resource}` : "/") +
+    (request.id ? "/:id" : "") +
+    (request.verb ? `/${request.verb}` : "");
+  const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
+  const main = document.getElementById("main-container");
+  main.innerHTML = screen.render();
+};
+
+window.addEventListener("load", router);
+window.addEventListener("hashchange", router);
