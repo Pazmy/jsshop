3981e00364daa2b5106afe8c2d5f2284ab56fb5e
Pazmy <fadhillah17azmy01@gmail.com>
2020-09-13 14:43:49 +0700

    fetch product from server

--- backend/server.js
+++ backend/server.js
@@ -8,5 +8,13 @@ app.use(cors());
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
+app.get("/api/product/:id", (req, res) => {
+  const product = data.products.find((x) => x._id == req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+});
 
 app.listen(5000, () => console.log("server running ..."));
