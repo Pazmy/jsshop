4ccc43053b6f831d2720a7dca5104195429c2749
Pazmy <fadhillah17azmy01@gmail.com>
2020-09-15 02:32:03 +0700

    create cart action

--- frontend/src/screen/CartScreen.js
+++ frontend/src/screen/CartScreen.js
@@ -1,4 +1,4 @@
-import { parseRequestUrl } from "../utils";
+import { parseRequestUrl, rerender } from "../utils";
 import { getProduct } from "../api";
 import { getCartItems, setCartItems } from "../localStorage";
 
@@ -6,16 +6,47 @@ const addToCart = (item, forceUpdate = false) => {
   let cartItems = getCartItems();
   const exitsItem = cartItems.find((x) => x.product === item.product);
   if (exitsItem) {
-    cartItems = cartItems.map((x) =>
-      x.product === exitsItem.product ? item : x
-    );
+    if (forceUpdate) {
+      cartItems = cartItems.map((x) =>
+        x.product === exitsItem.product ? item : x
+      );
+    }
   } else {
     cartItems = [...cartItems, item];
   }
   setCartItems(cartItems);
+  if (forceUpdate) {
+    rerender(CartScreen);
+  }
 };
+const removeFromCart = (id) => {
+  setCartItems(getCartItems().filter((x) => x.product !== id));
+  if (id === parseRequestUrl().id) {
+    location.hash = "/cart";
+  } else {
+    rerender(CartScreen);
+  }
+};
+
 const CartScreen = {
-  after_render: () => {},
+  after_render: () => {
+    const qtySelects = document.getElementsByClassName("qty-select");
+    Array.from(qtySelects).forEach((qtySelect) => {
+      qtySelect.addEventListener("change", (e) => {
+        const item = getCartItems().find((x) => x.product === qtySelect.id);
+        addToCart({ ...item, qty: Number(e.target.value) }, true);
+      });
+    });
+    const deleteBtns = document.getElementsByClassName("delete-button");
+    Array.from(deleteBtns).forEach((btn) => {
+      btn.addEventListener("click", () => {
+        removeFromCart(btn.id);
+      });
+    });
+    document.getElementById("checkout-button").addEventListener("click", () => {
+      location.hash = "/signin";
+    });
+  },
   render: async () => {
     const request = parseRequestUrl();
     if (request.id) {
@@ -41,7 +72,8 @@ const CartScreen = {
                 
                 ${
                   cartItems.length === 0
-                    ? '<div>Cart is empty. <a href="/#/>Go Shopping</a></div>'
+                    ? `<div>Cart is empty.
+                    <a href="/#/">Go Shopping</a></div>`
                     : cartItems
                         .map(
                           (item) => `
@@ -57,9 +89,21 @@ const CartScreen = {
                             </div>
                             <div>
                             Qty<select class="qty-select" id="${item.product}">
-                                    <option value="1">1</option>   
+                                    ${[
+                                      ...Array(item.countInStock).keys(),
+                                    ].map((x) =>
+                                      item.qty === x + 1
+                                        ? `<option selected value="${x + 1}">${
+                                            x + 1
+                                          }</option>`
+                                        : `<option value="${x + 1}">${
+                                            x + 1
+                                          }</option>`
+                                    )}
                                 </select>
-                                <button type="button "class="delete-button" id="${item.product}">
+                                <button type="button "class="delete-button" id="${
+                                  item.product
+                                }">
                                     Delete    
                                 </button>
                             </div>
